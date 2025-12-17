5d64dcd51eb101c13c0972b046de9b64e03c9fe5
MrTibbss <79116856+MrTibbss@users.noreply.github.com>
2025-12-16 20:30:05 -0500

    Add project configuration files

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#8a63d2',
+        secondary: '#6a4bc9',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
